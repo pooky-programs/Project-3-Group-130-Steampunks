714cc725cff4b32eea0384f595451249b18efb02
pooky-programs <smyers2@ufl.edu>
2024-04-22 07:57:08 -0500

    update merge.py (used in another directory to generate final_data)

--- src/merge.py
+++ src/merge.py
@@ -4,10 +4,15 @@ import csv
 table1 = pd.read_csv('spydata.csv')
 table2 = pd.read_csv('steamdata.csv')
 
+table1 = table1.drop_duplicates(subset=['appid'], keep='first')
+table2 = table2.drop_duplicates(subset=['appid'], keep='first')
+
 # full outer join
 merged = pd.merge(table1, table2, on='appid', how='outer')
 
 merged = merged[['appid', 'name', 'type', 'genre', 'metacritic', 'developer', 'positive','negative','owners', 'initialprice','ccu']]
-# merged.drop(columns=['recommendations'], inplace=True)
 
-merged.to_csv('merged_data.csv', index=False)
+for col in merged.columns:
+    merged[col] = merged[col].apply(lambda x: str(x).replace('"', '') if pd.notnull(x) else '')
+
+merged.to_csv('merged_data_test.csv', index=False)
